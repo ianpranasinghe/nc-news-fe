de69ef93b11350c6522385a1055dace561481206
Ian Ranasinghe <ianranasinghe@hotmail.co.uk>
2019-09-18 09:35:44 +0100

    Lots of changes, forgot to commit, whoops- Articles List, Article, and Topic lists functional, voting system works but no live update, working on sortby

--- nc-news-fe/src/App.js
+++ nc-news-fe/src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Router } from "@reach/router";
+import Navigation from "./components/Navigation";
+import Home from "./components/Home";
+import Topics from "./components/Topics";
+import Article from "./components/Article";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation />
+      <Router>
+        <Home path="/" />
+        <Topics path="topics/:topic" />
+        <Article path="topics/:topic/:article_id" />
+        <Article path="/:article_id" />
+      </Router>
+    </>
   );
 }
 
